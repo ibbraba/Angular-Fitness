ac39475c4af81f68357ee10a8ab792ab97a790da
JuniorAwounfouet <juniorawounfouet@gmail.com>
2025-10-17 16:39:22 +0200

    init form components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { MeasureComponent } from './measure/measure.component';
+import { FoodComponent } from './food/food.component';
+import { ExerciceComponent } from './exercice/exercice.component';
 
  const routes: Routes = [
       { path: '',
         component: HomeComponent
       },
+      { path: 'measure',
+        component: MeasureComponent
+      },
+      { path: 'food',
+        component: FoodComponent
+      },
+      { path: 'exercise',
+        component: ExerciceComponent
+      }
 ];
 
 
