b2f480fac8ad610fd6fb4923ff7bc9b69c191f7b
JuniorAwounfouet <juniorawounfouet@gmail.com>
2025-10-25 23:14:35 +0200

    update food form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import { RegisterComponent } from './auth/register/register.component';
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -29,7 +30,8 @@ import { FooterComponent } from './footer/footer.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/food/food.component.ts
+++ src/app/food/food.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-food',
   templateUrl: './food.component.html',
   styleUrls: ['./food.component.scss']
 })
-export class FoodComponent {
+export class FoodComponent implements OnInit {
+
+  foodForm!: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+    private router: Router
+  ) { }
+
+  ngOnInit(): void {
+    this.foodForm = this.fb.group({
+      foodname: new FormControl('', [Validators.required, Validators.minLength(4)]),
+      proteins: new FormControl('', [Validators.required, Validators.min(4)]),
+      glucides: new FormControl('', Validators.required),
+      lipides: new FormControl('', Validators.required),
+    });
+  }
+
+  addFood() {
+    if (this.foodForm.invalid) return;
+    // this.authService.addUser({
+    //   username: this.foodForm.value.username,
+    //   password: this.foodForm.value.password
+    // });
+    console.log('Nourriture ajoutée avec succès !');
+    // this.router.navigate(['/login']);
+  }
+
+  get getErrorLabel() {
+    if (this.foodForm.errors?.['required']) return 'Les champs sont obligatoires';
+    // if (!!this.foodForm.controls?.['password']?.errors?.['minlength']) return `La longueur minimal pour votre mot de passe est ${this.foodForm.controls?.['password']?.errors?.['minlength']?.requiredLength}`;
+    // if (this.foodForm.errors?.['missMatch']) return 'Les mots de passe ne correspondent pas';
+    return 'Formulaire invalide';
+  }
 
 }
