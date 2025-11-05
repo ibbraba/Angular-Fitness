9ddddcedbc7ab83614611b3439cb0a185c18a469
ibbraba <ibbraba@gmail.com>
2025-11-05 13:59:14 +0100

    homepage + header and footer

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Observable, of } from 'rxjs';
 import { HomeHistoryService } from '../shared/home-history.service';
+import { AuthService } from '../auth/auth.service';
+import { FitnessService } from '../shared/services/fitness.service';
 
 @Component({
   selector: 'app-home',
@@ -10,23 +12,51 @@ import { HomeHistoryService } from '../shared/home-history.service';
 })
 export class HomeComponent implements OnInit {
 
-  constructor(private router: Router, private homeHistoryService: HomeHistoryService) { }
+  constructor(private router: Router, private homeHistoryService: HomeHistoryService, private authService: AuthService, private fitnessService: FitnessService) { }
+
 
   connectedUser: any = null; // Replace 'any' with your User type
   recapsHistory: any[] = [];
-  ngOnInit(): void {
-     this.homeHistoryService.getUserRecapHistory(1).subscribe(recaps => {
-      this.recapsHistory = recaps;
-    console.log(this.recapsHistory);
-  });
+ngOnInit(): void {
+
+  // Call this to trigger user retrieval if saved
+  const isConnected = this.authService.isUserConnected();
+
+  if (this.authService.user) {
+    // User was already in memory
+    this.connectedUser = this.authService.user;
+    this.loadRecap();
+  } else if (isConnected) {
+    // User is being restored via HTTP → wait for it
+    this.authService.getSavedUserInfo().subscribe(users => {
+      this.connectedUser = users[0];
+      this.authService.user = this.connectedUser; // store it back
+      this.loadRecap();
+    });
   }
+}
+
+loadRecap() {
+  if (!this.connectedUser) return;
+  
+  this.homeHistoryService.getUserRecapHistory(this.connectedUser.id).subscribe(recaps => {
+    this.recapsHistory = recaps;
+    console.log("Recaps loaded:", this.recapsHistory);
+  });
+}
 
 
-  navigateToFitness () {
+
+  navigateToFitness() {
     this.router.navigate(['/measure']);
   }
 
 
+  calculateCalories(proteins: number, glucides: number, lipides: number): number {
+    return this.fitnessService.calculateCalories(proteins, glucides, lipides);
+  }
 
-
+  calculateIMC(weight: number, height: number): number {
+    return this.fitnessService.calculateIMC(weight, height);
+  }
 }
